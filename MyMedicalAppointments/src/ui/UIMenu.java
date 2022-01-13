56a3a20c78617de6e9382c1495115d875a24a120
Epicode <gexone@gmail.com>
2022-01-12 20:29:47 -0600

    prueba

--- MyMedicalAppointments/src/ui/UIMenu.java
+++ MyMedicalAppointments/src/ui/UIMenu.java
@@ -15,7 +15,7 @@ public class UIMenu {
             System.out.println("1. model.Doctor");
             System.out.println("2. model.Patient");
             System.out.println("0. Salir");
-
+  
             Scanner sc = new Scanner(System.in);
             response = Integer.valueOf(sc.nextLine());
 
